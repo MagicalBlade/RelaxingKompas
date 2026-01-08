55c188b9d21592229b5ca2a192c9050a4b755c31
Александр Воскобойник <magical.blade@gmail.com>
2026-01-08 09:20:01 +0300

    оформление

--- FormWeightAndSize.Designer.cs
+++ FormWeightAndSize.Designer.cs
@@ -65,7 +65,7 @@ namespace RelaxingKompas
             // 
             // b_ok
             // 
-            this.b_ok.Location = new System.Drawing.Point(158, 183);
+            this.b_ok.Location = new System.Drawing.Point(191, 179);
             this.b_ok.Name = "b_ok";
             this.b_ok.Size = new System.Drawing.Size(75, 23);
             this.b_ok.TabIndex = 1;
@@ -79,13 +79,13 @@ namespace RelaxingKompas
             this.tb_thickness.Location = new System.Drawing.Point(103, 153);
             this.tb_thickness.Margin = new System.Windows.Forms.Padding(5);
             this.tb_thickness.Name = "tb_thickness";
-            this.tb_thickness.Size = new System.Drawing.Size(56, 20);
+            this.tb_thickness.Size = new System.Drawing.Size(60, 20);
             this.tb_thickness.TabIndex = 0;
             this.tb_thickness.TextChanged += new System.EventHandler(this.tb_thickness_TextChanged);
             // 
             // tb_density
             // 
-            this.tb_density.Location = new System.Drawing.Point(387, 104);
+            this.tb_density.Location = new System.Drawing.Point(442, 105);
             this.tb_density.Margin = new System.Windows.Forms.Padding(5);
             this.tb_density.Name = "tb_density";
             this.tb_density.Size = new System.Drawing.Size(77, 20);
@@ -96,10 +96,10 @@ namespace RelaxingKompas
             // tb_weight
             // 
             this.tb_weight.BackColor = System.Drawing.Color.Red;
-            this.tb_weight.Location = new System.Drawing.Point(378, 153);
+            this.tb_weight.Location = new System.Drawing.Point(419, 153);
             this.tb_weight.Margin = new System.Windows.Forms.Padding(5);
             this.tb_weight.Name = "tb_weight";
-            this.tb_weight.Size = new System.Drawing.Size(46, 20);
+            this.tb_weight.Size = new System.Drawing.Size(50, 20);
             this.tb_weight.TabIndex = 3;
             this.tb_weight.TextChanged += new System.EventHandler(this.tb_WeightAndSize_TextChanged);
             this.tb_weight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tb_weight_MouseDown);
@@ -107,43 +107,43 @@ namespace RelaxingKompas
             // l_thickness
             // 
             this.l_thickness.AutoSize = true;
-            this.l_thickness.Location = new System.Drawing.Point(102, 129);
+            this.l_thickness.Location = new System.Drawing.Point(93, 129);
             this.l_thickness.Name = "l_thickness";
             this.l_thickness.Padding = new System.Windows.Forms.Padding(3);
-            this.l_thickness.Size = new System.Drawing.Size(59, 19);
+            this.l_thickness.Size = new System.Drawing.Size(81, 19);
             this.l_thickness.TabIndex = 4;
-            this.l_thickness.Text = "Толщина";
+            this.l_thickness.Text = "Толщина, мм";
             this.l_thickness.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // l_density
             // 
             this.l_density.AutoSize = true;
-            this.l_density.Location = new System.Drawing.Point(392, 79);
+            this.l_density.Location = new System.Drawing.Point(430, 80);
             this.l_density.Name = "l_density";
             this.l_density.Padding = new System.Windows.Forms.Padding(3);
-            this.l_density.Size = new System.Drawing.Size(67, 19);
+            this.l_density.Size = new System.Drawing.Size(100, 19);
             this.l_density.TabIndex = 5;
-            this.l_density.Text = "Плотность";
+            this.l_density.Text = "Плотность, кг/м³";
             // 
             // l_weight
             // 
             this.l_weight.AutoSize = true;
-            this.l_weight.Location = new System.Drawing.Point(378, 129);
+            this.l_weight.Location = new System.Drawing.Point(413, 129);
             this.l_weight.Name = "l_weight";
             this.l_weight.Padding = new System.Windows.Forms.Padding(3);
-            this.l_weight.Size = new System.Drawing.Size(46, 19);
+            this.l_weight.Size = new System.Drawing.Size(63, 19);
             this.l_weight.TabIndex = 6;
-            this.l_weight.Text = "Масса";
+            this.l_weight.Text = "Масса, кг";
             // 
             // l_yardage
             // 
             this.l_yardage.AutoSize = true;
-            this.l_yardage.Location = new System.Drawing.Point(32, 56);
+            this.l_yardage.Location = new System.Drawing.Point(20, 56);
             this.l_yardage.Name = "l_yardage";
             this.l_yardage.Padding = new System.Windows.Forms.Padding(3);
-            this.l_yardage.Size = new System.Drawing.Size(60, 19);
+            this.l_yardage.Size = new System.Drawing.Size(85, 19);
             this.l_yardage.TabIndex = 7;
-            this.l_yardage.Text = "Площадь";
+            this.l_yardage.Text = "Площадь, мм²";
             // 
             // tb_yardage
             // 
@@ -209,42 +209,42 @@ namespace RelaxingKompas
             // tb_width
             // 
             this.tb_width.BackColor = System.Drawing.Color.Red;
-            this.tb_width.Location = new System.Drawing.Point(169, 153);
+            this.tb_width.Location = new System.Drawing.Point(185, 153);
             this.tb_width.Margin = new System.Windows.Forms.Padding(5);
             this.tb_width.Name = "tb_width";
-            this.tb_width.Size = new System.Drawing.Size(49, 20);
+            this.tb_width.Size = new System.Drawing.Size(65, 20);
             this.tb_width.TabIndex = 13;
             this.tb_width.TextChanged += new System.EventHandler(this.tb_WeightAndSize_TextChanged);
             // 
             // tb_length
             // 
             this.tb_length.BackColor = System.Drawing.Color.Red;
-            this.tb_length.Location = new System.Drawing.Point(228, 153);
+            this.tb_length.Location = new System.Drawing.Point(262, 153);
             this.tb_length.Margin = new System.Windows.Forms.Padding(5);
             this.tb_length.Name = "tb_length";
-            this.tb_length.Size = new System.Drawing.Size(58, 20);
+            this.tb_length.Size = new System.Drawing.Size(65, 20);
             this.tb_length.TabIndex = 14;
             this.tb_length.TextChanged += new System.EventHandler(this.tb_WeightAndSize_TextChanged);
             // 
             // l_width
             // 
             this.l_width.AutoSize = true;
-            this.l_width.Location = new System.Drawing.Point(167, 129);
+            this.l_width.Location = new System.Drawing.Point(180, 129);
             this.l_width.Name = "l_width";
             this.l_width.Padding = new System.Windows.Forms.Padding(3);
-            this.l_width.Size = new System.Drawing.Size(52, 19);
+            this.l_width.Size = new System.Drawing.Size(74, 19);
             this.l_width.TabIndex = 15;
-            this.l_width.Text = "Ширина";
+            this.l_width.Text = "Ширина, мм";
             // 
             // l_length
             // 
             this.l_length.AutoSize = true;
-            this.l_length.Location = new System.Drawing.Point(234, 129);
+            this.l_length.Location = new System.Drawing.Point(260, 129);
             this.l_length.Name = "l_length";
             this.l_length.Padding = new System.Windows.Forms.Padding(3);
-            this.l_length.Size = new System.Drawing.Size(46, 19);
+            this.l_length.Size = new System.Drawing.Size(68, 19);
             this.l_length.TabIndex = 16;
-            this.l_length.Text = "Длина";
+            this.l_length.Text = "Длина, мм";
             // 
             // tb_pos
             // 
@@ -269,7 +269,7 @@ namespace RelaxingKompas
             // tb_sheet
             // 
             this.tb_sheet.BackColor = System.Drawing.Color.Red;
-            this.tb_sheet.Location = new System.Drawing.Point(434, 153);
+            this.tb_sheet.Location = new System.Drawing.Point(479, 153);
             this.tb_sheet.Margin = new System.Windows.Forms.Padding(5);
             this.tb_sheet.Name = "tb_sheet";
             this.tb_sheet.Size = new System.Drawing.Size(53, 20);
@@ -279,7 +279,7 @@ namespace RelaxingKompas
             // l_sheet
             // 
             this.l_sheet.AutoSize = true;
-            this.l_sheet.Location = new System.Drawing.Point(432, 129);
+            this.l_sheet.Location = new System.Drawing.Point(477, 129);
             this.l_sheet.Name = "l_sheet";
             this.l_sheet.Padding = new System.Windows.Forms.Padding(3);
             this.l_sheet.Size = new System.Drawing.Size(56, 19);
@@ -289,7 +289,7 @@ namespace RelaxingKompas
             // tb_steel
             // 
             this.tb_steel.BackColor = System.Drawing.Color.Red;
-            this.tb_steel.Location = new System.Drawing.Point(296, 153);
+            this.tb_steel.Location = new System.Drawing.Point(337, 153);
             this.tb_steel.Margin = new System.Windows.Forms.Padding(5);
             this.tb_steel.Name = "tb_steel";
             this.tb_steel.Size = new System.Drawing.Size(72, 20);
@@ -299,7 +299,7 @@ namespace RelaxingKompas
             // l_steel
             // 
             this.l_steel.AutoSize = true;
-            this.l_steel.Location = new System.Drawing.Point(311, 129);
+            this.l_steel.Location = new System.Drawing.Point(352, 129);
             this.l_steel.Name = "l_steel";
             this.l_steel.Padding = new System.Windows.Forms.Padding(3);
             this.l_steel.Size = new System.Drawing.Size(43, 19);
@@ -308,7 +308,7 @@ namespace RelaxingKompas
             // 
             // b_settings
             // 
-            this.b_settings.Location = new System.Drawing.Point(409, 12);
+            this.b_settings.Location = new System.Drawing.Point(455, 12);
             this.b_settings.Name = "b_settings";
             this.b_settings.Size = new System.Drawing.Size(75, 23);
             this.b_settings.TabIndex = 18;
@@ -319,7 +319,7 @@ namespace RelaxingKompas
             // b_Cancel
             // 
             this.b_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.b_Cancel.Location = new System.Drawing.Point(243, 183);
+            this.b_Cancel.Location = new System.Drawing.Point(276, 179);
             this.b_Cancel.Name = "b_Cancel";
             this.b_Cancel.Size = new System.Drawing.Size(75, 23);
             this.b_Cancel.TabIndex = 1;
@@ -343,7 +343,7 @@ namespace RelaxingKompas
             // lb_steel
             // 
             this.lb_steel.FormattingEnabled = true;
-            this.lb_steel.Location = new System.Drawing.Point(296, 70);
+            this.lb_steel.Location = new System.Drawing.Point(332, 70);
             this.lb_steel.Name = "lb_steel";
             this.lb_steel.Size = new System.Drawing.Size(83, 56);
             this.lb_steel.TabIndex = 20;
@@ -377,7 +377,7 @@ namespace RelaxingKompas
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.b_Cancel;
-            this.ClientSize = new System.Drawing.Size(496, 213);
+            this.ClientSize = new System.Drawing.Size(542, 213);
             this.Controls.Add(this.cb_savedxf);
             this.Controls.Add(this.cb_savefragment);
             this.Controls.Add(this.lb_steel);
